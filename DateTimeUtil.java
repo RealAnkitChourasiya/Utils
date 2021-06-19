2ea18dcfe717349907cb88d02829f9c2389c856c
RealAnkitChourasiya <59200381+RealAnkitChourasiya@users.noreply.github.com>
2021-06-19 13:14:01 +0530

    Update DateTimeUtil.java

--- DateTimeUtil.java
+++ DateTimeUtil.java
@@ -27,22 +27,35 @@ public class DateTimeUtil {
 		Integer minute = localTime.getMinute();
 		
 		String word[] = {"", "one", "two", "three", "four", "five", "six", "Seven", "eight", "nine", "ten", "eleven", "twelve"};
+		String tens[] = {"", "ten", "twenty", "thirty", "forty", "fifty"};
 		
-		if(LocalTime.MIDNIGHT.toString().equals(hour+":"+minute)) {
+		if(LocalTime.MIDNIGHT.toString().equals(hour+"0:"+minute+0)) {
 			return "It's Midnight";
 		}
-		if(LocalTime.NOON.toString().equals(hour+":"+minute)) {
+		if(LocalTime.NOON.toString().equals(hour+"0:"+minute+0)) {
 			return "It's Midday";
 		}
 		
 		String m = minute.toString();
+		String a = ""; String b = "";
 		if(m.length() == 2) {
-			String a = word[Integer.parseInt(m.substring(0,1))];
-			String b = word[Integer.parseInt(m.substring(1))];
+			a = word[Integer.parseInt(m.substring(0,1))];
+			b = word[Integer.parseInt(m.substring(1))];
 			m =a + " " + b;
+		} else {
+			m = word[minute];
 		}
-		
-		String time = "It's " +  word[hour] + " " + m;
+		String time;
+		if(hour > 12) {
+			if(minute%10 == 0) {
+				time = "It's " +  word[hour-12] + " :" + tens[minute/10];
+			} else {
+				time = "It's " +  word[hour-12] + " :" + m;
+			}
+			
+		} else {
+			time = "It's " +  word[hour] + " :" + m;
+		}
 		
 		return time;
 	}
